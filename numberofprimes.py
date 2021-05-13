434bc383e3266f105f1477aae5242c03367ee5a6
harsha16208 <sriharsha16208@gmail.com>
2021-05-13 16:53:38 +0530

    Add files via upload

---
+++ numberofprimes.py
@@ -0,0 +1,13 @@
+import math
+prime=['','']
+for i in range(2,int(input())+1):
+    prime.append(True)
+for j in range(2,int(math.sqrt(len(prime)-1))+1):
+    if(prime[j]==True):
+        for k in range((j*j),len(prime),j):
+            prime[k]=False
+length=0
+for i in range(2,len(prime)):
+    if prime[i]==True:
+        length+=1
+print(length)
