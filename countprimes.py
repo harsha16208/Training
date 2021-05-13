f63aadaa12def7f26a1b4e2d4a581d301f84fbad
harsha16208 <sriharsha16208@gmail.com>
2021-05-13 17:18:33 +0530

    Add files via upload

---
+++ countprimes.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        prime=['','']
+        for i in range(2,n+1):
+            prime.append(True)
+        for j in range(2,int(math.sqrt(len(prime)-1))+1):
+            if(prime[j]==True):
+                for k in range((j*j),len(prime),j):
+                    prime[k]=False
+        length=0
+        for i in range(2,len(prime)):
+            if prime[i]==True:
+                length+=1
+        return length
