0967790615e03676eb4605e58786686457c4d923
harsha16208 <sriharsha16208@gmail.com>
2021-05-14 18:52:00 +0530

    Update numberofprimes.py

--- numberofprimes.py
+++ numberofprimes.py
@@ -3,7 +3,7 @@ prime=['','']
 for i in range(2,int(input())+1):
     prime.append(True)
 for j in range(2,int(math.sqrt(len(prime)-1))+1):
-    if(prime[j]==True):
+    if prime[j]==True:
         for k in range((j*j),len(prime),j):
             prime[k]=False
 length=0
